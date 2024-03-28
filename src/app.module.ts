e89c1084682322527213394503d32fe7a060bbae
zura <zura.baramia98@outlook.com>
2024-03-28 20:09:29 +0400

    vercel

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { CarsModule } from './cars/cars.module';
-
-const corsOptions = {
-  origin: [
-    'https://grumpy-baseball-cap-colt.cyclic.app/',
-    'https://studio.apollographql.com',
-  ],
-  // Add other allowed origins as needed
-  credentials: true, // Allow cookies for authentication purposes
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Allowed HTTP methods
-  allowedHeaders:
-    'Content-Type, Accept, Authorization, Origin, X-Requested-With', // Allowed headers
-};
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [
@@ -26,12 +16,13 @@ const corsOptions = {
       useFactory: () => ({
         autoSchemaFile: 'src/schema.gql',
         playground: true,
-        cors: corsOptions,
+        introspection: true,
+        graphiql: true,
       }),
     }),
     CarsModule,
   ],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    allowedHeaders:
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    maxAge: 3600,
+    exposedHeaders: 'Authorization',
+  });
   await app.listen(3000);
 }
 bootstrap();
